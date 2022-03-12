b471da282f3bc9f8213d1fae94ad234e8c32961a
Stuart <stu.con85@gmail.com>
2022-03-12 00:00:43 +0000

    latest commit with near working dist

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -27,33 +26,3 @@ app.listen(port, () => {
 
 
 
-=======
-const express = require('express');
-const app = express();
-const port = 3000;
-app.use(express.static('public'));
-
-
-
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/public/index.html');
-  
-
-  res.set({
-    'Content-Type': 'text/html',
-    'Access-Control-Allow-Origin': '*'
-  })
-
-
-});
-
-
-
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
-
->>>>>>> 4408848e9adb988838a0c9131dff0d90ca7af3c6
