507c9e2a91f1df4c53b313941cff8c1eb2e07134
Stuart Connolly <stu.con85@gmail.com>
2022-03-14 17:40:01 +0000

    push changes from laptop

--- public/javascript/pedalboard.js
+++ public/javascript/pedalboard.js
@@ -37,7 +37,7 @@ const track = audioContext.createMediaElementSource(audioElement);
 
   
 
-//GAIN NODE - to be made into a boost pedal?
+// /////////////////////////   Boost    ///////////////////////////////
 
 const gainNode = audioContext.createGain();
 const volumeControl = document.querySelector('#volume');
@@ -47,6 +47,64 @@ volumeControl.addEventListener('input', function(event) {
  const target = event.target   //target is on volumeControl
 gainNode.gain.value = target.value; 
 });
+
+
+
+const boostNode = audioContext.createGain();
+
+//grab boost pedal buttons
+const boostLedLight = document.querySelector('#boostLedLight');
+const boostOnSwitch = document.querySelector('#boostOnSwitch');
+const boostVolume = document.querySelector('#boostVolume');
+
+
+//add the event listeners to run the distortion functions
+boostOnSwitch.addEventListener('click', handleBoostOnOff); 
+
+let boostActive = false;
+
+function handleBoostOnOff() {
+
+if (boostActive == false) {
+
+    const boostValue = boostVolume.value;
+    boostNode.gain.value = boostValue;
+    boostActive = true;
+
+    //grabs the BOOST VOLUME slider
+    boostVolume.addEventListener('input', handleBoostAmount);
+
+    //targets the value of the DISTORTION TONE slider and changes it
+    function handleBoostAmount(event) {
+        if (boostActive == true) {
+            const target = event.target; //grabs the value of the event on the distortionTone event listener
+            const value = target.value;  //puts value of target into variable to use
+            boostNode.gain.value = value;
+        } else if (boostActive == false) {
+            boostNode.gain.value = 2;
+
+        } //end if statement
+    } // end handleBoostAmount func
+} //end if statement 
+else if (boostActive == true) {
+
+    //turn pedal off
+    boostActive = false;
+   boostNode.gain.value = 2;
+} // end if statement
+ 
+
+
+    //turns the LED light on/off
+    if (boostLedLight.className == 'ledLightOff') {
+        boostLedLight.className = 'ledLightOn';
+    } else {
+        boostLedLight.className = 'ledLightOff';
+    }
+} // end func
+
+
+
 
 
 
@@ -60,79 +118,73 @@ gainNode.gain.value = target.value;
 const distortionNode = audioContext.createWaveShaper();
 
 //grab distortion pedal buttons
-const ledLightOff = document.querySelector('#ledLightOff');
+const distortionLedLight = document.querySelector('#distortionLedLight');
 const distortionOnSwitch = document.querySelector('#distortionOnSwitch');
 const distortionTone = document.querySelector('#distortionTone');
 
 //add the event listeners to run the distortion functions
-distortionOnSwitch.addEventListener('click', handleDistortionOnOff); //can put in variables if needed?
+distortionOnSwitch.addEventListener('click', handleDistortionOnOff); 
 
 
 let distortionActive = false;  
 // turns on and off the distortion 
 function handleDistortionOnOff() {
-
-
-
+    //checks if pedal is turned on
     if (distortionActive == false) {
+
     //accesses the value of the DISTORTION TONE slider
-    //switches pedal ON at whatever tone slider is set at
     const distortionValue = distortionTone.value;
     distortionNode.curve = makeDistortionCurve(distortionValue); 
-    //console.log(distortionValue);
+    distortionNode.oversample = "4x";
+
+    ///switches pedal ON at whatever value tone slider is set at
     distortionActive = true; 
-    //console.log("active turned to true");
+
     //grabs the DISTORTION TONE slider
     distortionTone.addEventListener('input', handleDistortionAmount);
 
     //targets the value of the DISTORTION TONE slider and changes it
     function handleDistortionAmount(event) {
-
-        const target = event.target; //grabs the value of the event on the distortionTone event listener
-        const value = target.value;  //puts value of target into variable to use
-
-        distortionNode.curve = makeDistortionCurve(parseInt(value));
-   } // end handleDistAmount func
+        if (distortionActive == true) {
+            const target = event.target; //grabs the value of the event on the distortionTone event listener
+            const value = target.value;  //puts value of target into variable to use
+            distortionNode.curve = makeDistortionCurve(parseInt(value));
+        } else if (distortionActive == false) {
+            distortionNode.curve = makeDistortionCurve(0); 
+        } //end if statement
+    } // end handleDistAmount func
 } //end if statement 
 else if (distortionActive == true) {
 
     //turn pedal off
    distortionActive = false;
-   console.log("active turned to false");
    distortionNode.curve = makeDistortionCurve(0);
-    //const distortionOutput ='.connect(distortionNode)';
-
-   // distortionOutput ='';
-
 }
-
-
-
     //turns the LED light on/off
-    if (ledLightOff.id == 'ledLightOff') {
-        ledLightOff.id = 'ledLightOn';
+    if (distortionLedLight.className == 'ledLightOff') {
+        distortionLedLight.className = 'ledLightOn';
     } else {
-        ledLightOff.id = 'ledLightOff';
+        distortionLedLight.className = 'ledLightOff';
     }
 
 
 } // end handleDistortionOn func
 
 
-
-
-
-
 // this function sets the actual amount of distortion to be applied
 function makeDistortionCurve(amount) {
-    
-    let n_samples = 100, curve = new Float32Array(n_samples);
-    for (let i = 0 ; i < n_samples; ++i ) {
-        let x = i * 2 / n_samples - 1;
-        curve[i] = (Math.PI + amount) * x / (Math.PI + amount * Math.abs(x));
+    var k = typeof amount === 'number' ? amount : 50,
+      n_samples = 44100,
+      curve = new Float32Array(n_samples),
+      deg = Math.PI / 180,
+      i = 0,
+      x;
+    for (; i < n_samples; ++i) {
+      x = (i * 2) / n_samples - 1;
+      curve[i] = ((3 + k) * x * 20 * deg) / (Math.PI + k * Math.abs(x));
     }
     return curve;
-} 
+  }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -150,11 +202,11 @@ function makeDistortionCurve(amount) {
 
 /////////////////////////   This is the chain that connects it all together   ///////////////////////////////
 
-track.connect(distortionNode);
-distortionNode.connect(gainNode);
-gainNode.connect(audioContext.destination);
+// track.connect(distortionNode);
+// distortionNode.connect(gainNode);
+// gainNode.connect(audioContext.destination);
 
 
-track.connect(distortionNode).connect(gainNode).connect(audioContext.destination);
+track.connect(gainNode).connect(boostNode).connect(distortionNode).connect(audioContext.destination);
 
 //${distortionOutput}
